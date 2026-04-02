7076cb4e7ffe4b8c66c4c1d9c6657bde54304ae4
Chia20-1 <chiachun2024@gmail.com>
2026-04-02 14:13:56 +0800

    First commit

---
+++ ex0/ft_command_quest.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+import sys
+
+
+def ft_command_quest() -> None:
+    length: int = len(sys.argv)
+    print("=== Command Quest ===")
+    print(f"Program name: {sys.argv[0]}")
+    if (length == 1):
+        print("No arguments provided!")
+    else:
+        print(f"Arguments received: {length - 1}")
+        for i in range(1, length):
+            print(f"Argument {i}: {sys.argv[i]}")
+    print(f"Total argument: {len(sys.argv)}")
+
+
+# def ft_command_quest() -> None:
+#     args = sys.argv[1:]
+#     for i, arg in enumerate(args, start=1):
+#         print(f"Argument {i}: {arg}")
+
+
+if __name__ == "__main__":
+    ft_command_quest()
