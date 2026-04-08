16874fc14766e8b7d32f045d23b96b401c8f2298
Chia20-1 <chiachun2024@gmail.com>
2026-04-08 20:45:02 +0800

    Pseudocode

---
+++ ex4/ft_inventory_system.py
@@ -0,0 +1,11 @@
+import sys
+
+````
+1. I can split my input with colons .split(:)
+2. Specify max split time. FormatError(Error - invalid parameter: ).
+2a. Have an index to track which one are looping through so I can do argv[index]
+3. int() the value to check. ValueError.
+4. I have to construct dictionary. Then another layer to check duplicate.
+5. Dictionary has an in-built method to find duplicate
+
+````
